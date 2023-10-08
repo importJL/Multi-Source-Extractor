5fbd1eb643d896b759601c80188dd590b7215908
importJL <91000291+importJL@users.noreply.github.com>
2023-10-08 16:58:18 +0800

    Add files via upload

--- multiextractor/constants.py
+++ multiextractor/constants.py
@@ -1,5 +1,9 @@
 import pymongo
 import spacy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 CATEGORY_MAP = {
     'general': 'general',
@@ -39,3 +43,72 @@ class SciDailyConstants:
     SCI_DOMAIN_NAME = 'https://www.sciencedaily.com'
     SCI_URL_FULL = f'{SCI_DOMAIN_NAME}/news/computers_math/artificial_intelligence/'
     SCI_HEADERS = {'User-Agent': 'Chrome/116.0.5845.110  Safari/18615.2.9.11.10'}
+    
+
+class DBConstLoader:
+    _NEON_CONN_STR = 'postgresql+psycopg2://{DB_USER}:{DB_KEY}@{COMPUTE_NAME}.{COMPUTE_LOCATION}.aws.neon.tech/{DB_NAME}'
+    _MONGO_CONN_STR = 'mongodb+srv://{DB_USER}:{DB_KEY}@{DB_NAME}.l0wmrp9.mongodb.net/?retryWrites=true&w=majority'
+    _COCKROACH_CONN_STR = 'postgresql://{DB_USER}:{DB_KEY}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+    def __init__(self, data_name: str):
+        self.DB_KEY = None
+        self.DB_USER = None
+        self.COMPUTE_NAME = None
+        self.COMPUTE_LOCATION  = None
+        self.DB_NAME = None
+        self.DB_PORT = None
+        self.DB_HOST = None
+        self.data_name = data_name
+        self._get_params()
+        self.conn_str = self._build_conn_str()
+        
+    def _get_params(self):
+        match self.data_name.lower():
+            case ('gnews' | 'scidaily'):
+                self.DB_KEY = os.getenv('NEON_CONN_KEY')
+                self.DB_USER = os.getenv('NEON_USER')
+                self.COMPUTE_NAME = os.getenv('NEON_COMPUTE')
+                self.COMPUTE_LOCATION = os.getenv('NEON_LOCATION')
+                self.DB_NAME = os.getenv('NEON_DB')
+                self.DB_PORT = os.getenv('NEON_PORT')
+                self.DB_HOST = f'{self.COMPUTE_NAME}.{self.COMPUTE_LOCATION}.aws.neon.tech'
+            case 'alphavan':
+                self.DB_USER = os.getenv('ATLAS_USER')
+                self.DB_KEY = os.getenv('ATLAS_KEY')
+                self.DB_NAME = os.getenv('ATLAS_DB')
+            case ('trade_econ' | 'cnbc'):
+                self.DB_USER = os.getenv('COCKROACH_USER')
+                self.DB_KEY = os.getenv('COCKROACH_PW')
+                self.DB_HOST = os.getenv('COCKROACH_HOST')
+                self.DB_PORT = os.getenv('COCKROACH_PORT')
+                self.DB_NAME = os.getenv('COCKROACH_DB')
+            case _:
+                return Exception('No paramaters found for connection.')
+    
+    def _build_conn_str(self):
+        match self.data_name.lower():
+            case 'gnews' | 'scidaily':
+                _str = self._NEON_CONN_STR.format(
+                    DB_USER=self.DB_USER,
+                    DB_KEY=self.DB_KEY,
+                    COMPUTE_NAME=self.COMPUTE_NAME,
+                    COMPUTE_LOCATION=self.COMPUTE_LOCATION,
+                    DB_NAME=self.DB_NAME,
+                )
+            case 'alphavan':
+                _str = self._MONGO_CONN_STR.format(
+                    DB_USER=self.DB_USER,
+                    DB_KEY=self.DB_KEY,
+                    DB_NAME=self.DB_NAME
+                )
+            case 'trade_econ' | 'cnbc':
+                _str = self._COCKROACH_CONN_STR.format(
+                    DB_USER=self.DB_USER,
+                    DB_KEY=self.DB_KEY,
+                    DB_HOST=self.DB_HOST,
+                    DB_PORT=self.DB_PORT,
+                    DB_NAME=self.DB_NAME
+                )
+            case _:
+                raise Exception('Connection string cannot be built.')
+        return _str
--- multiextractor/query.py
+++ multiextractor/query.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from multiextractor.apis.db import neondb_connection
+from multiextractor.apis.db import neondb_connection, pg_connection
+from multiextractor.constants import DBConstLoader
 
 
-def sql_insert_articles(df: pd.DataFrame, constraint_col: str | None):
+def sql_insert_articles(df: pd.DataFrame, constraint_col: str | None, conn_params: DBConstLoader | None = None):
     '''
     Creates and executes SQL script for article insertion
     
@@ -10,6 +11,7 @@ def sql_insert_articles(df: pd.DataFrame, constraint_col: str | None):
     df: DataFrame object - table containing data to be inserted
     constraint_col: str - column name indicating key column upon detecting duplicattes
     '''
+    # conn = pg_connection(conn_params)
     conn = neondb_connection()
     cur = conn.cursor()
     
@@ -38,7 +40,7 @@ def sql_insert_articles(df: pd.DataFrame, constraint_col: str | None):
         print(e)
         print('Error encountered in article insertion process')
 
-def sql_create_table(df: pd.DataFrame, table_name: str = 'articles', unique_col: str | None = None):
+def sql_create_table(df: pd.DataFrame, table_name: str = 'articles', unique_col: str | None = None, conn_params: DBConstLoader | None = None):
     '''
     Creates and executes SQL script for article table creation
     
@@ -46,6 +48,7 @@ def sql_create_table(df: pd.DataFrame, table_name: str = 'articles', unique_col:
     df: DataFrame object - data table to extract table schema for SQL table creation query
     table_name: str - name of table to be created in Postgres database
     unique_col: str - name of column to set as unique index in created table
+    conn_params: DBConstLoader object - preset connection parameters based on data source loading
     '''
     table_script = pd.io.sql.get_schema(df, table_name)
     idx = table_script.index('TABLE')
@@ -60,6 +63,7 @@ def sql_create_table(df: pd.DataFrame, table_name: str = 'articles', unique_col:
         table_script = table_script[:idx] + ' UNIQUE' + table_script[idx:]
 
     # print(table_script)
+    # with pg_connection(conn_params) as conn:
     with neondb_connection() as conn:
         try:
             cur = conn.cursor()
