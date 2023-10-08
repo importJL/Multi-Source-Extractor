c6f85ed9cbff0f2e445f0c46ba6a770387e1d14a
importJL <91000291+importJL@users.noreply.github.com>
2023-10-08 16:58:49 +0800

    Add files via upload

--- multiextractor/apis/db.py
+++ multiextractor/apis/db.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 import psycopg2
 import pymongo
 import os
+from multiextractor.constants import DBConstLoader
 
 load_dotenv()
 
@@ -17,10 +18,35 @@ MONGO_DB_USER = os.getenv('ATLAS_USER')
 MONGO_DB_KEY = os.getenv('ATLAS_KEY')
 MONGO_DB_NAME = os.getenv('ATLAS_DB')
 
+COCKROACH_USER = os.getenv('COCKROACH_USER')
+COCKROACH_PW = os.getenv('COCKROACH_PW')
+COCKROACH_HOST = os.getenv('COCKROACH_HOST')
+COCKROACH_PORT = os.getenv('COCKROACH_PORT')
+COCKROACH_DB = os.getenv('COCKROACH_DB')
+
 NEON_CONN_STR = f'postgresql+psycopg2://{NEON_DB_USER}:{NEON_DB_KEY}@{NEON_COMPUTE_NAME}.{NEON_LOCATION}.aws.neon.tech/{NEON_DB_NAME}'
 MONGO_CONN_STR = f'mongodb+srv://{MONGO_DB_USER}:{MONGO_DB_KEY}@{MONGO_DB_NAME}.l0wmrp9.mongodb.net/?retryWrites=true&w=majority'
-# MONGO_CONN_STR = f'mongodb://{MONGO_DB_USER}:{MONGO_DB_KEY}@{MONGO_DB_NAME}.l0wmrp9.mongodb.net/?retryWrites=true&w=majority'
+COCKROACH_CONN_STR = f'postgresql://{COCKROACH_USER}:{COCKROACH_PW}@{COCKROACH_HOST}:{COCKROACH_PORT}/{COCKROACH_DB}'
 
+
+def pg_connection(conn_params: DBConstLoader) -> psycopg2.extensions.connection:
+    '''Initiates backend PostgreSQL database connection'''
+    # engine = create_engine(conn_params.conn_str)
+    conn = psycopg2.connect(host=conn_params.DB_HOST, 
+                            dbname=conn_params.DB_NAME, 
+                            user=conn_params.DB_USER, 
+                            password=conn_params.DB_KEY, 
+                            port=conn_params.DB_PORT)
+    return conn
+
+# def mongodb_connection(conn_params: DBConstLoader) -> pymongo.mongo_client.MongoClient:
+#     '''Initiates backend MongoDB Atlas database connection'''
+#     return pymongo.MongoClient(conn_params.conn_str)
+
+
+# def mongodb_get_db(client: pymongo.mongo_client.MongoClient, conn_params: DBConstLoader) -> pymongo.database.Database:
+#     '''Enables MongoDB client to respective database'''
+#     return client[conn_params.DB_NAME]
 
 def neondb_connection() -> psycopg2.extensions.connection:
     '''Initiates backend PostgreSQL database connection via NEON DB host'''
@@ -33,8 +59,10 @@ def neondb_connection() -> psycopg2.extensions.connection:
     return conn
 
 def mongodb_connection() -> pymongo.mongo_client.MongoClient:
-   return pymongo.MongoClient(MONGO_CONN_STR)
+    '''Initiates backend MongoDB Atlas database connection'''
+    return pymongo.MongoClient(MONGO_CONN_STR)
 
 
 def mongodb_get_db(client: pymongo.mongo_client.MongoClient) -> pymongo.database.Database:
+    '''Enables MongoDB client to respective database'''
     return client[MONGO_DB_NAME]
--- multiextractor/apis/news.py
+++ multiextractor/apis/news.py
@@ -1,10 +1,17 @@
 from dotenv import load_dotenv
 import urllib.request
+import feedparser
+import pandas as pd
+import tldextract
+from urllib.parse import urlparse
+from datetime import date, time
 import os
 import json
 import requests
+import asyncio
 from bs4 import BeautifulSoup
-from multiextractor.transforms.soup_funcs import locate_elements
+from concurrent.futures import ProcessPoolExecutor
+from multiextractor.transforms.soup_funcs import locate_elements, process_text
 
 load_dotenv()
 
@@ -43,3 +50,49 @@ def extract_content(url: str, element_search: str, method: str, headers: dict[st
     r = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(r.content, 'html5lib')
     return locate_elements(soup, element_search, method)
+
+def extract_rss_body(url, body_attrs=None, list_attrs=None) -> str:
+    ''''''
+    _body_kwargs, _list_kwargs = {}, {}
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    
+    if body_attrs is not None: _body_kwargs.update({'attrs': body_attrs})
+    _art_body = locate_elements(soup, 'div', 'find', **_body_kwargs)
+    
+    if list_attrs is not None: _list_kwargs.update({'attrs': list_attrs})
+    _body_list = process_text(_art_body, 'summary', 'find', 'div', **_list_kwargs)
+    return ' '.join(_body_list)
+
+async def parallel_rss_extract(executor: ProcessPoolExecutor, entry: object, body_attrs: dict[str, str] | None, list_attrs: dict[str, str] | None):
+    ''''''
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(
+        executor,
+        extract_rss_body,
+        entry.link, 
+        body_attrs, 
+        list_attrs
+    ) if (body_attrs is not None) & (list_attrs is not None) else entry.summary
+    
+async def populate_data_struct(executor: ProcessPoolExecutor, entry: object, body_attrs: dict[str, str] | None, list_attrs: dict[str, str] | None):
+    _summary = await parallel_rss_extract(executor, entry, body_attrs, list_attrs)
+    return {
+        'title': entry.title,
+        'description': entry.summary,
+        'content': _summary,
+        'url': entry.link,
+        'image': '',
+        'publishedAt': entry.published,
+        'name': tldextract.extract(entry.link).domain.title(),
+        'domainName': urlparse(entry.link).netloc,
+        'publishedDate': date(entry.published_parsed.tm_year, entry.published_parsed.tm_mon, entry.published_parsed.tm_mday),
+        'publishedTime': time(entry.published_parsed.tm_hour, entry.published_parsed.tm_min, entry.published_parsed.tm_sec)
+    }   
+
+async def create_entry_from_rss(url: str, executor: ProcessPoolExecutor, body_attrs: dict[str, str] | None = None, list_attrs: dict[str, str] | None = None) -> pd.DataFrame:
+    _feeds = feedparser.parse(url)
+    _entry_list = await asyncio.gather(*(populate_data_struct(executor, _entry, body_attrs, list_attrs) for _entry in _feeds.entries))
+    _df = pd.DataFrame(_entry_list)
+    _df['publishedAt'] = pd.to_datetime(_df['publishedAt']).dt.strftime('%Y%m%d %H:%M:%S%z+00:00')
+    return _df
