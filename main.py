d7f46fa82b576d98cc28a0bcc185ca4135104e9f
importJL <91000291+importJL@users.noreply.github.com>
2023-11-30 13:37:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import polars as pl
 import multiextractor
 from multiextractor import SciDailyConstants as sci
 from multiextractor import DBConstLoader
@@ -137,6 +138,88 @@ def alphavan_load(
     if df_stock_prices is not None:
         multiextractor.insert_to_collection(db_name, 'alphav_daily_price', df_stock_prices.to_dict(orient='records'), set_index=[('date', 1), ('last_refreshed', 1), ('ticker', 1)], unique=True, ordered=False)
 
+async def cnbc_rss_extract(pool_num: int = 8):
+    url = 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=19854910'
+    df = await multiextractor.create_entry_from_rss(url, pool_num, body_attrs={'class': 'ArticleBody-articleBody'}, list_attrs={'class': 'group'})
+    return df
+
+def cnbc_rss_transform(df: pd.DataFrame):
+    tmp = df.copy()
+    tmp = multiextractor.process_datetime(tmp)
+    tmp = multiextractor.process_sentence_count(tmp, multiextractor.SPACY_NLP, 'title', 'description', 'content')
+    tmp = multiextractor.process_token_count(tmp, multiextractor.SPACY_NLP, 'title', 'description', 'content')
+    return tmp
+
+def cnbc_rss_load(df: pd.DataFrame):
+    conn_params = multiextractor.DBConstLoader('cnbc')
+    multiextractor.sql_create_table(df, conn_params, table_name='cnbc_articles', unique_col='title')
+    multiextractor.sql_insert_articles(df, conn_params, table_name='cnbc_articles', constraint_col='title')
+    
+async def trade_econ_extract(pool_num: int = 8):
+    url = 'https://tradingeconomics.com/rss/news.aspx?i=bank+lending+rate'
+    df = await multiextractor.create_entry_from_rss(url, pool_num)
+    return df
+
+def trade_econ_transform(df: pd.DataFrame):
+    tmp = df.copy()
+    tmp = multiextractor.process_datetime(tmp)
+    tmp = multiextractor.process_sentence_count(tmp, multiextractor.SPACY_NLP, 'title', 'description', 'content')
+    tmp = multiextractor.process_token_count(tmp, multiextractor.SPACY_NLP, 'title', 'description', 'content')
+    return tmp
+
+def trade_econ_load(df: pd.DataFrame, redis_hash_idx_name: str = 'teblr'):
+    conn_params = multiextractor.DBConstLoader('trade_econ')
+    tmp = multiextractor.dt_to_isoformat(df)
+    records = multiextractor.create_redis_records(tmp)
+    multiextractor.key_val_insert(records, conn_params)
+    
+def iqair_extract(country: str, state: str, city: str):
+    iqa_countries = multiextractor.IQAirBuilder('countries')
+    iqa_states = multiextractor.IQAirBuilder('states')
+    iqa_cities = multiextractor.IQAirBuilder('cities')
+    iqa_data = multiextractor.IQAirBuilder('data')
+    
+    iqa_countries.request_data()
+    iqa_states.request_data(country=country)
+    iqa_cities.request_data(country=country, state=state)
+    iqa_data.request_data(country=country, state=state, city=city)
+    return iqa_countries, iqa_states, iqa_cities, iqa_data
+
+def iqair_transform(iqair_obj: multiextractor.IQAirBuilder, resp_data: str | None = None):
+    tmp_recs = iqair_obj.process(resp_data)
+    df_proc = multiextractor.reformat_iqair(tmp_recs)
+    return df_proc
+
+def openweather_extract(country: str, city: str, limit_geo_resp: int = 5):
+    ow_geo = multiextractor.OpenWeatherBuilder('geoloc')
+    ow_forecast = multiextractor.OpenWeatherBuilder('forecast')
+    ow_current = multiextractor.OpenWeatherBuilder('current')
+    
+    ow_geo.request_data(city=city, country=country, limit=limit_geo_resp)
+    ow_geo.process()
+    assert (ow_geo.selected_lat is not None) & (ow_geo.selected_lon is not None), 'Latitudinal and longitudinal coordinates are not provided'
+    search_coords = {'lat': ow_geo.selected_lat, 'lon': ow_geo.selected_lon}
+    ow_forecast.request_data(**search_coords)
+    ow_current.request_data(**search_coords)
+    return ow_forecast, ow_current
+
+def openweather_transform(ow_obj: multiextractor.OpenWeatherBuilder, resp_data: str | None = None):
+    tmp_recs = ow_obj.process(resp_data)
+    match ow_obj.category:
+        case 'forecast':
+            df_proc = multiextractor.reformat_forecasted_ow(tmp_recs)
+        case 'current':
+            df_proc = multiextractor.reformat_current_ow(tmp_recs)
+        case _:
+            raise Exception('Cannot process data due to incorrect selection of available data category')
+    return df_proc
+
+def bq_load(data: pl.DataFrame):
+    bqclient = multiextractor.BQClimate()
+    bqclient.create_bq_schema(data.schema)
+    bqclient.load_table_to_gcp(data, src_format='pandas')
+
+
 if __name__ == '__main__':
     df_articles = gnews_extract()
     df_articles = gnews_transform(df_articles)
@@ -149,6 +232,26 @@ if __name__ == '__main__':
     ticker_prices, news, yield_data, inflation_data = alphavan_extract('AAPL')
     sent_data_list, article_base_list, sentiments_list, topics_list, df_daily_treasury, df_annual_inflation, df_stock_prices = alphavan_transform(ticker_prices, news, yield_data, inflation_data)
     alphavan_load(sent_data_list, article_base_list, sentiments_list, topics_list, df_daily_treasury, df_annual_inflation, df_stock_prices)
+    
+    cnbc_rss = cnbc_rss_extract()
+    cnbc_rss = cnbc_rss_transform(cnbc_rss)
+    cnbc_rss_transform(cnbc_rss)
+    
+    trade_econ_blr = trade_econ_extract()
+    trade_econ_blr = trade_econ_transform(trade_econ_blr)
+    trade_econ_load(trade_econ_blr)
+
+    country, state, city = 'Canada', 'British Columbia', 'Vancouver BC'
+    iqa_countries, iqa_states, iqa_cities, iqa_data = iqair_extract(country, state, city)
+    df_iqa_data = iqair_transform(iqa_data)
+    bq_load(df_iqa_data)
+    
+    country, city = 'HK', 'Hong Kong'
+    ow_forecast, ow_current = openweather_extract(country, city)
+    df_ow_forecast = openweather_transform(ow_forecast)
+    df_ow_current = openweather_transform(ow_current)
+    bq_load(df_ow_forecast)
+    bq_load(df_ow_current)
 
 # client = utils.mongodb_connection()
 # db_name = utils.mongodb_get_db(client)
